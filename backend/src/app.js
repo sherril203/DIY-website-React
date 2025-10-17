ab9b9235afce8ca7a9e402cf85c7eeb1472cd7c8
sherril203 <sherrilkumar2003@gmail.com>
2025-10-17 22:17:50 +0530

    files

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,10 @@ const router = require('./Routes/routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://artsworld.vercel.app', // Allow frontend domain
+  credentials: true // If you're using cookies or auth headers
+}));
 app.use(express.json());
 app.use(router);
 app.use('/files', express.static(path.join(__dirname, 'products')));
